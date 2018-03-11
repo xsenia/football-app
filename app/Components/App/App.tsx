9e068b647d66e8032447468afb4d74eb68d923d8
Ksenia <xsanka30@gmail.com>
2018-03-11 13:13:57 +0500

    add key, fixed header

--- app/Components/App/App.tsx
+++ app/Components/App/App.tsx
@@ -4,24 +4,46 @@ import {headers, data, leagues} from "../../Helpers/Data";
 export class App extends React.Component {
     constructor(props: any, state: any){
         super(props, state);
-        this.state = {};
+        const dataClone = JSON.parse(JSON.stringify(data));
+        this.state = {
+            data: dataClone
+        };
     }
+
+
 
     private renderHeader(): JSX.Element {
         return(
-            <thead>
+            <tr>
                 {headers.map((head, i) =>
-                    <th>{head}</th> 
+                    <th key={i}>{head}</th>
                 )}
-            </thead>
+            </tr>
         );
     }
+
+    /*renderBody(): JSX.Element {
+        const data = this.state.data;
+        console.log(data);
+        return(
+            {data.map((cell, i) =>
+                <tr key={i}>
+                    <td>{cell.eventInfo}</td>
+                    <td>{cell.info}</td>
+                    <td>{cell.results}</td>
+                </tr>
+            )}
+        );
+    }*/
 
     render(): JSX.Element {
         return(
             <div>
                 <table>
-                    {this.renderHeader()}
+                    <tbody>
+                        {this.renderHeader()}
+                       {/* {this.renderBody()}*/}
+                    </tbody>
                 </table>
             </div>
         )
