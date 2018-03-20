e6cd6b8369936fbc79fc1cffb128b7e6911c9a34
Ksenia <xsanka30@gmail.com>
2018-03-20 22:51:22 +0500

    search - worked click

--- app/Components/App/Search.tsx
+++ app/Components/App/Search.tsx
@@ -6,18 +6,29 @@ interface AppProps {
     sortByIncreasing: any;*/
 }
 
-export class Search extends React.Component<AppProps, { searchString:string}> {
+export class Search extends React.Component<AppProps, { searchString:string,classNameList:string}> {
     constructor(props: any, state: any){
         super(props, state);
         this.state = {
-            searchString: ''/*,
-            visibleList: false*/
+            searchString: '',
+            classNameList: 'invisibleList' //visibleList
         };
     }
 
 
     private handleChange = (event:any) => {
-        this.setState({searchString:event.target.value});
+        this.setState({
+            searchString:event.target.value,
+            classNameList: 'visibleList'
+        });
+    }
+
+    private handleList = (elem:string) => {
+        console.log(elem);
+        this.setState({
+            searchString: elem,
+            classNameList: 'invisibleList'
+        });
     }
 
     render(): JSX.Element {
@@ -34,14 +45,17 @@ export class Search extends React.Component<AppProps, { searchString:string}> {
 
         return(
             <div className="b-search">
-                <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Type here" />
+                <input
+                    type="text"
+                    value={this.state.searchString}
+                    onChange={this.handleChange}
+                    placeholder="Лига"
+                />
 
-                <ul>
-
+                <ul className={this.state.classNameList}>
                     { libraries.map((elem:any, i:number)=>{
-                        return <li key={i}>{elem}</li>
+                        return <li key={i} onClick={() => this.handleList(elem)}>{elem}</li>
                     }) }
-
                 </ul>
 
             </div>
